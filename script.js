6971bd1f2ff914e91247128e4e49e5166eb8c1aa
basten72000 <119486417+basten72000@users.noreply.github.com>
2023-05-19 15:34:19 +0200

    add fonction add card

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-document.body.onload=function(){
- 
-}
 
 
+function add_card(title, iner, bgcolor) {
+    var body_content = document.getElementsByClassName("body-content")[0];
+  
+    var ul = document.createElement('ul');
+    ul.className = 'toDo';
+  
+    var li = document.createElement('li');
+  
+    var divHead = document.createElement('div');
+    divHead.className = 'head';
+    divHead.textContent = title;
+    divHead.style.backgroundColor = bgcolor; 
+    
+    var divContent = document.createElement('div');
+    divContent.className = 'content';
+    divContent.textContent = iner; 
+    
+    var divCheck = document.createElement('div');
+    divCheck.id = 'check';
+  
+    
+    li.appendChild(divHead);
+    li.appendChild(divContent);
+    li.appendChild(divCheck);
+  
+    ul.appendChild(li);
+  
+    body_content.appendChild(ul);
+  }
+  
 
+  
 
-
-
+  document.body.onload=function(){
+    add_card("", "", '#347cb5');
+  }
